0fc668dcf9c123bb50b684151f0c7ed5402548e2
Carl Egge <carl.egge@web.de>
2023-03-21 15:27:15 +0100

    create 1000 per minute ratelimit using slowapi pkg

--- server/app/main.py
+++ server/app/main.py
@@ -2,19 +2,34 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+# Slowapi is a library to add rate limiting to your FastAPI application.
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+
 from app.config import get_config
 from app.db import db
 from app.rest import contracts
 
+# create the app
+app = FastAPI(title="Repository Smart Contracts")
+
+
+# add rate limiting
+limiter = Limiter(key_func=get_remote_address, headers_enabled=True, default_limits=["1000/minute"])
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
+
+# add cors
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
 ]
-
-app = FastAPI(title="Repository Smart Contracts")
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -24,6 +39,7 @@ app.add_middleware(
     expose_headers=["*"],
 )
 
+# create base router
 app.include_router(contracts.router, prefix='/api/contracts', tags=["Contracts"],)
 
 
